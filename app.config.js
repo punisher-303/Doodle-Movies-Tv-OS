e5471144b38cd79d18b9e3825ba991c9c9d72675
punisher-303 <mehafin0@gmail.com>
2026-01-24 16:30:56 +0530

    fix for playstore

--- app.config.js
+++ app.config.js
@@ -109,7 +109,7 @@ module.exports = () => {
         minSdkVersion: 24,
         edgeToEdgeEnabled: true,
         package: 'com.doodle.movies',
-        versionCode: 200190,
+        versionCode: 200191,
         permissions: [
           'FOREGROUND_SERVICE',
           'FOREGROUND_SERVICE_MEDIA_PLAYBACK',
